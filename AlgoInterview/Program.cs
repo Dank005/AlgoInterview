23d225a22e102a40839f0c3332753083e797c1e1
Daniil Yushkov <dyushkov@in.top>
2025-04-29 23:53:52 +0500

    IsAnagram242

--- AlgoInterview/Program.cs
+++ AlgoInterview/Program.cs
@@ -1,3 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(ContainsDuplicate217([1, 2, 3, 4]));
+        Console.WriteLine(ContainsDuplicate217([1, 2, 3, 1]));
+        
+        Console.WriteLine(IsAnagram242("anagram", "nagaram"));
+        Console.WriteLine(IsAnagram242("rat", "car"));
+        
+    }
 
-Console.WriteLine("Hello, World!");
+    private static bool ContainsDuplicate217(int[] nums)
+    {
+        var hashSet = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (hashSet.Contains(num))
+                return true;
+            
+            hashSet.Add(num);
+        }
+        
+        return false;
+        
+        //OR return nums.Length != nums.Distinct().Count();
+    }
+    
+    private static bool IsAnagram242(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        var dictS = new Dictionary<char, int>();
+        var dictT = new Dictionary<char, int>();
+        
+        for (var i = 0; i < s.Length; i++)
+        {
+            if(dictS.ContainsKey(s[i]))
+                dictS[s[i]]++;
+            else
+                dictS.Add(s[i], 1);
+            
+            if(dictT.ContainsKey(t[i]))
+                dictT[t[i]]++;
+            else
+                dictT.Add(t[i], 1);
+        }
+
+        foreach (var d in dictS)
+        {
+            if(!dictT.ContainsKey(d.Key) || dictT[d.Key] != d.Value)
+                return false;
+        }
+        
+        return true;
+    }
+}
